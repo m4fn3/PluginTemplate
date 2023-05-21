95a2835e6769dc4aee5994734810db58ea5c81d4
mafu <mafusukee@gmail.com>
2023-05-21 23:56:20 +0900

    PluginTemplate

---
+++ create.py
@@ -0,0 +1,35 @@
+import json
+import os
+import shutil
+
+print("--- Enmity Plugin Template of @m4fn3---")
+print("[-] Enter the information of a plugin.")
+plugin_name = input("name >")
+if os.path.isdir(f"./{plugin_name}"):
+    raise RuntimeError(f"Project '{plugin_name}' already exists!")
+plugin_description = input("description >")
+plugin_color = input("color (Optional: #000000) >")
+
+print(f"[*] Creating a project folder to ./{plugin_name}")
+shutil.copytree("./template", f"./{plugin_name}")
+
+manifest: dict
+with open(f"./{plugin_name}/manifest.json", "r") as f:
+    manifest = json.load(f)
+    manifest["name"] = plugin_name
+    manifest["description"] = plugin_description
+    if plugin_color:
+        manifest["color"] = plugin_color
+with open(f"./{plugin_name}/manifest.json", "w") as f:
+    json.dump(manifest, f, indent=2)
+
+paths = ["src/index.tsx", "src/components/Settings.tsx"]
+for path in paths:
+    text: str
+    with open(f"./{plugin_name}/{path}", "r") as f:
+        text = f.read()
+    text = text.replace("PLUGIN_NAME", plugin_name)
+    with open(f"./{plugin_name}/{path}", "w") as f:
+        f.write(text)
+
+print(f"[o] Done!")
---
+++ template/src/index.tsx
@@ -0,0 +1,24 @@
+import {Plugin, registerPlugin} from 'enmity/managers/plugins'
+import {React} from 'enmity/metro/common'
+import {create} from 'enmity/patcher'
+// @ts-ignore
+import manifest, {name as plugin_name} from '../manifest.json'
+import Settings from "./components/Settings"
+
+const Patcher = create('PLUGIN_NAME')
+
+const PLUGIN_NAME: Plugin = {
+    ...manifest,
+    onStart() {
+
+    },
+    onStop() {
+        Patcher.unpatchAll()
+    }
+    ,
+    getSettingsPanel({settings}) {
+        return <Settings settings={settings}/>
+    }
+}
+
+registerPlugin(PLUGIN_NAME)
